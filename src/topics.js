1da130f3486fa4d5eb4a57dcf94308380faefde4
orionmelt <orionmelt@gmail.com>
2017-11-20 16:20:18 -0700

    Added more topics

--- src/topics.js
+++ src/topics.js
@@ -1,6 +1,9 @@
 const topics = [
   {id: 'python', name: 'Python'},
-  {id: 'js', name:'JavaScript'}
+  {id: 'js', name:'JavaScript'},
+  {id: 'ds', name: 'Data Structures'},
+  {id: 'algo', name: 'Algorithms'},
+  {id: 'java', name: 'Java'},
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default topics;
